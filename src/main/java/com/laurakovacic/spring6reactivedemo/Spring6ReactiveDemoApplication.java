87a3daabdc4a94f20fd92e363fe04df8f6b913ba
Laura <laurakovacic@outlook.com>
2023-04-08 21:01:11 +0200

    initial commit

---
+++ src/main/java/com/laurakovacic/spring6reactivedemo/Spring6ReactiveDemoApplication.java
@@ -0,0 +1,13 @@
+package com.laurakovacic.spring6reactivedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6ReactiveDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6ReactiveDemoApplication.class, args);
+    }
+
+}
